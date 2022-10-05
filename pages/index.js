fc1c280e27b31df49959e786c3ad0ac1923c5af5
andres-hub <andrescalderoncl@gmail.com>
2022-10-05 16:36:04 -0500

    ok 2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import supabase from '../utils/supabase'
 
-export function getStaticProps() {
+export async function getStaticProps() {
+
+  const {data: posts, error} = await supabase.from('posts').select('*')
+
+  if(error){
+    throw new Error(error);
+  }
+
   return {
     props:{
-      posts: []
+      posts
     }
   }  
 }
--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export default createClient(
-    process.NEXT_PUBLIC_SUPABASE_URL, 
-    process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+    'https://zymaejgqpmhaywnaqzjd.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bWFlamdxcG1oYXl3bmFxempkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUwMDAzMzQsImV4cCI6MTk4MDU3NjMzNH0.gBPeHoBve1iiX4mHKBd4wY0Tw-VWmSYL8CPgs7O3SCU'
 );
